dbf59e98a531f348d02d092b42e1eaf7d32a4e38
Cyrus Cowley <cyficowley@gmail.com>
2018-10-12 23:37:45 -0700

    inital strucutre

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../css/main.css';
 
-class App extends Component {
+class Main extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -25,4 +23,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Main;

ec225db2689af8055f0d327e2ebfb03eebe5a29f
Ian Carrasco <iansee.c@gmail.com>
2018-10-13 23:36:31 -0700

    Accounts upload

--- src/app.py
+++ src/app.py
@@ -1,8 +1,12 @@
 import scraper
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
+
 api = Api(app)
  
 class getData(Resource):
@@ -12,5 +16,5 @@ class getData(Resource):
 api.add_resource(getData, '/scraper/<userName>')
 
 if __name__ == '__main__':
-     app.run()
+     app.run(debug=True)
 

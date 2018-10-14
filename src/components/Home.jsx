3139cf9bf1cea64fcf57e4bbd1b1af85a3c75d46
Surya Krishnan <skrishnan99@icloud.com>
2018-10-14 00:55:57 -0700

    can now scrape famous people

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,9 +7,8 @@ import {Link} from 'react-router-dom'
 class Home extends Component {
   render() {
     return (
-		<div className="Login" style={{margin:"0px", padding:"0px"}}>
+		<div className="Login" style={{marginLeft:"0px", padding:"10px"}}>
             <h3 style={{float:"left", padding:"0px", marginLeft:"0px", marginTop:"0px", fontSize:"1.5rem"}}> Fuck you!! </h3>
-            <h3 style={{float:"right", padding:"0px", fontSize:"1.5rem"}}> Ugly Pic </h3>
 		</div>
     );
 	}

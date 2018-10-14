b078167a38803d17d53cfa57235df756bc536c1d
Surya Krishnan <skrishnan99@icloud.com>
2018-10-13 19:44:18 -0700

    implemented some flask stuff

---
+++ src/app.py
@@ -0,0 +1,15 @@
+import scraper
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+ 
+class getData(Resource):
+    def get(self, userName):
+        return {'data': scraper.scrape(userName)}
+
+api.add_resource(getData, '/scraper/<userName>')
+
+if __name__ == '__main__':
+     app.run()
